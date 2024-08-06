41444bcad7a951fa72676f2cbdf6c66e8be797e1
Andrew Rozdolsky <arozdolskiy@digital-spirit.ru>
2024-08-06 22:40:45 +0400

    bor response request query

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 // Import the framework and instantiate it
+import "dotenv/config";
 import Fastify from "fastify";
+import { Telegraf } from "telegraf";
+
 const fastify = Fastify({
   logger: true,
 });
+const bot = new Telegraf(process.env.API_TELEGRAM_BOT_KEY);
+
+// bot.on("message", async (ctx) => {
+//   console.log(ctx.message);
+// });
 
 // Declare a route
-fastify.get("/", async function handler(request, reply) {
-  return { hello: "world" };
-});
+// fastify.get("/", async function handler(request, reply) {
+//   bot.telegram.sendMessage(
+//     process.env.ID_TELEGRAM_CHAT,
+//     request
+//   );
+//   return { hello: "world" };
+// });
 
-fastify.post("/webhook", async function handler(request, reply) {
-  console.log(request.body);
+fastify.get("/webhook", async function handler(request, reply) {
+  bot.telegram.sendMessage(
+    process.env.ID_TELEGRAM_CHAT,
+    `${JSON.stringify(request.query)}`
+  );
   return { hello: "world" };
 });
 
@@ -21,3 +36,5 @@ try {
   fastify.log.error(err);
   process.exit(1);
 }
+
+bot.launch();
